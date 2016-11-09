8572f8a7721155f54c8974420f101d67f693cd9a
liangwenzhu <826415551@qq.com>
2016-11-09 17:06:42 +0800

    demo3

--- js/base.js
+++ js/base.js
@@ -46,37 +46,6 @@ var mySwiper = new Swiper('.swiper1',{
 
     }
 });
-var swiper2 = new Swiper('.swiper2', {
-    effect: 'flip',
-    autoplay:6000,
-    speed:800,
-    slidesPerView: 3,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    autoplayDisableOnInteraction:false,
-
-});
-
-
-var swiper3 = new Swiper('.swiper3', {
-    autoplay:6000,
-    speed:800,
-    slidesPerView: 4,
-    spaceBetween: 10,
-    nextButton: '#swiper3-slide-right',
-    prevButton: '#swiper3-slide-left',
-    autoplayDisableOnInteraction:false,
-    paginationClickable: true,
-});
-var swiper4 = new Swiper('.swiper4', {
-    pagination: '.swiper-pagination',
-    autoplay:3000,
-    speed:800,
-    slidesPerView: 3,
-    spaceBetween: 30,
-    autoplayDisableOnInteraction:false,
-});
-
 $(function(){
     var hide = function(){
     $("span:contains(产品简介2)").hide();
